df8acc6d62288640ea210549cd10c3b00f0663a5
chenrenbing <chenrenbing@meituan.com>
2022-01-02 21:06:05 +0800

    add fix

--- src/main/java/com/craftinginterpreters/lox/Lox.java
+++ src/main/java/com/craftinginterpreters/lox/Lox.java
@@ -13,6 +13,13 @@ public class Lox {
     public static void error(int line,String message){
         report(line,"",message);
     }
+    public static void error(Token token, String message) {
+        if (token.type == TokenType.EOF) {
+            report(token.line, " at end", message);
+        } else {
+            report(token.line, " at '" + token.lexeme + "'", message);
+        }
+    }
     public  static void report(int line,String where,String message){
         System.err.println("[line "+line+"] Error "+ where + ":"+message);
         hadError = true;
